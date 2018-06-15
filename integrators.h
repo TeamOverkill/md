ab600bc4fe1dce693d9185acd30d9f93c5ad4066
Samuel Stenberg <samuel.stenberg@teokem.lu.se>
2018-06-15 19:57:31 +0200

    - Fixed non-constant energy bug - Added variables to base - Cleaned up the code

--- integrators.h
+++ integrators.h
@@ -15,29 +15,22 @@ namespace integrators{
         Eigen::Vector3d force;
 
         for(int i = 0; i < base::numOfAtoms; i++){
-            //atoms[i]->pos += atoms[i]->vel * tStep + 0.5 * atoms[i]->force * tStep * tStep;
             if(atoms[i]->pos[0] >= base::boxDim - atoms[i]->radius){
-                //atoms[i]->pos[0] -= tStep * atoms[i]->vel[0] + 0.5 * atoms[i]->force[0] * tStep * tStep;
                 atoms[i]->vel[0] *= -1;
             }
-            if(atoms[i]->pos[0] <= atoms[i]->radius){
-                //atoms[i]->pos[0] -= tStep * atoms[i]->vel[0] + 0.5 * atoms[i]->force[0] * tStep * tStep;
+            if(atoms[i]->pos[0] <= atoms[i]->radius){ ;
                 atoms[i]->vel[0] *= -1;
             }
             if(atoms[i]->pos[1] >= base::boxDim){
-                //atoms[i]->pos[1] -= tStep * atoms[i]->vel[1] + 0.5 * atoms[i]->force[1] * tStep * tStep;
                 atoms[i]->vel[1] *= -1;
             }
             if(atoms[i]->pos[1] <= atoms[i]->radius){
-                //atoms[i]->pos[1] -= tStep * atoms[i]->vel[1] + 0.5 * atoms[i]->force[1] * tStep * tStep;
                 atoms[i]->vel[1] *= -1;
             }
             if(atoms[i]->pos[2] >= base::boxDim - atoms[i]->radius){
-                //atoms[i]->pos[2] -= tStep * atoms[i]->vel[2] + 0.5 * atoms[i]->force[2] * tStep * tStep;
                 atoms[i]->vel[2] *= -1;
             }
             if(atoms[i]->pos[2] <= atoms[i]->radius){
-                //atoms[i]->pos[2] -= tStep * atoms[i]->vel[2] + 0.5 * atoms[i]->force[2] * tStep * tStep;
                 atoms[i]->vel[2] *= -1;
             }
             atoms[i]->vel += 0.5 * tStep * atoms[i]->oldForce;
@@ -52,8 +45,6 @@ namespace integrators{
         */
 
         for(int i = 0; i < base::numOfAtoms; i++){
-            //atoms[i]->vel += 0.5 * (atoms[i]->oldForce + atoms[i]->force) * tStep;
-            //atoms[i]->oldForce = atoms[i]->force;
             atoms[i]->vel += 0.5 * tStep * atoms[i]->force;
             atoms[i]->oldForce = atoms[i]->force;
         }
